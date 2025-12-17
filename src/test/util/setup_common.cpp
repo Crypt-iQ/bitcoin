2b84554326d3944283b4cbadfdce8ecd8a42f338
Eugene Siegel <elzeigel@gmail.com>
2025-12-17 10:09:28 -0500

    test: disable check_block_index

--- src/test/util/setup_common.cpp
+++ src/test/util/setup_common.cpp
@@ -301,7 +301,7 @@ ChainTestingSetup::ChainTestingSetup(const ChainType chainType, TestOpts opts)
         ChainstateManager::Options chainman_opts{
             .chainparams = chainparams,
             .datadir = m_args.GetDataDirNet(),
-            .check_block_index = 1,
+            .check_block_index = 0,
             .notifications = *m_node.notifications,
             .signals = m_node.validation_signals.get(),
             // Use no worker threads while fuzzing to avoid non-determinism
